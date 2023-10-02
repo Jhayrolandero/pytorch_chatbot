c6fb96948cdce8b7f8d35e0d5436b166d7d8372a
Cjay Landero <xjaylandero@gmail.com>
2023-10-02 21:31:44 +0800

    some tweaks

--- chat.py
+++ chat.py
@@ -4,16 +4,15 @@ import json
 import torch
 
 from model import NeuralNet
-from main import bag_of_words, tokenize
+from utils import bag_of_words, tokenize
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-with open('intents.json', 'r') as json_data:
+with open('pytorch_chatbot/intents.json', 'r') as json_data:
     intents = json.load(json_data)
 
-FILE = "data.pth"
+FILE = "pytorch_chatbot/data.pth"
 data = torch.load(FILE)
-
 input_size = data["input_size"]
 hidden_size = data["hidden_size"]
 output_size = data["output_size"]
@@ -25,7 +24,7 @@ model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
 
-bot_name = "Hitler"
+bot_name = "Jarvis"
 print("Let's chat! (type 'quit' to exit)")
 while True:
     # sentence = "do you use credit cards?"
